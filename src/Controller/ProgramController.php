f76da6c2b94b41d6c60a41d582cd7cf4b60ffc3a
JennyViannay <jenny.viannay75@gmail.com>
2021-01-25 15:13:57 +0100

    init wild-series until quete 16

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,105 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use App\Entity\Episode;
+use App\Entity\Program;
+use App\Entity\Season;
+use App\Form\ProgramType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ProgramController
+ * @package App\Controller
+ * @Route("/programs", name="program_")
+ */
+class ProgramController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        return $this->render('program/index.html.twig', [
+            'programs' => $programs,
+        ]);
+    }
+
+    /**
+     * The controller for the program add form
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    {
+        // Create a new Category Object
+        $program = new Program();
+        // Create the associated Form
+        $form = $this->createForm(ProgramType::class, $program);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($program);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('program_index');
+        }
+        // Render the form
+        return $this->render('program/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/", name="show", methods={"GET"}, requirements={"id"="\d+"})
+     * @return Response
+     */
+    public function show(Program $program): Response
+    {
+        $seasons = $program->getSeasons();
+
+        return $this->render('program/show.html.twig', [
+            'program' => $program,
+            'seasons' => $seasons,
+        ]);
+    }
+
+    /**
+     * @Route("/{program}/seasons/{season}", name="season_show", methods={"GET"})
+     * @return Response
+     */
+    public function showSeason(Program $program, Season $season): Response
+    {
+        return $this->render('program/season_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+        ]);
+    }
+
+    /**
+     * @Route("/{program}/seasons/{season}/episodes/{episode}", name="episode_show", methods={"GET"})
+     * @return Response
+     */
+    public function showEpisode(Program $program, Season $season, Episode $episode): Response
+    {
+        return $this->render('program/episode_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episode' => $episode,
+        ]);
+    }
+}
